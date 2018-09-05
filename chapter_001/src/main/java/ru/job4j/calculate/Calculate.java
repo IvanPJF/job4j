640a85ffd833b9a4eda96601c0d7abc390ea0cbf
IvanPJF <teaching-light@yandex.ru>
2018-09-05 18:44:39 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j.calculate;
 
+/** Calculate Класс для вычисления арифметических операций
+ *@author IvanPJF
+ *@since 04.09.2018
+ *@version 0.1
+ */
 public class Calculate{
+	
+	/** main метод выводит на экран строку "Hello World"
+	 *@param args массив текстовых строк
+	 */
 	public static void main(String[] args){
 		System.out.println("Hello World");
 	}

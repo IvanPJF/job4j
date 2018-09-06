1aaefc9463bff89b66c75816fb19fa125ea4a8ff
IvanPJF <teaching-light@yandex.ru>
2018-09-06 14:35:22 +0300

    JUnit

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,8 +1,8 @@
 package ru.job4j.calculate;
 
 /** Calculate Класс для вычисления арифметических операций
- *@author IvanPJF
- *@since 04.09.2018
+ *@author IvanPJF (teaching-light@yandex.ru)
+ *@since 06.09.2018
  *@version 0.1
  */
 public class Calculate{
@@ -12,5 +12,14 @@ public class Calculate{
 	 */
 	public static void main(String[] args){
 		System.out.println("Hello World");
+	}
+	
+	/**
+	 * Method echo.
+	 * @param name Your name.
+	 * @return Echo plus your name.
+	 */
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
 	}
 }
---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**Тест
+ *@author IvanPJF (teaching-light@yandex.ru)
+ *@since 06.09.2018
+ *@version 0.1
+ */
+public class CalculateTest {
+	/**
+	 * Test echo.
+	 */
+	@Test
+	public void whenTakeNameThenTreeEchoPlusName() {
+		String input = "Ivan Krinitsyn";
+		String expect = "Echo, echo, echo : Ivan Krinitsyn"; 
+		Calculate calc = new Calculate();
+		String result = calc.echo(input);
+		assertThat(result, is(expect));
+	}
+ 
+}

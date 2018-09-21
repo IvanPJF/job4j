d32a54b1c72f895fdae35d5db5ab29f64cd8cf8e
IvanPJF <teaching-light@yandex.ru>
2018-09-21 11:04:13 +0300

    Крестики-нолики Коррекция 0.1

--- chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
+++ chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -3,7 +3,7 @@ package ru.job4j.tictactoe;
 /**Проверка логики игры.
  *@author IvanPJF (teaching-light@yandex.ru)
  *@since 20.09.2018
- *@version 0.1
+ *@version 0.2
  */
 public class Logic3T {
     private final Figure3T[][] table;
@@ -13,16 +13,27 @@ public class Logic3T {
     }
 
     /**
-     * Проверяет наличие в поле выигрышных комбинаций для крестика.
+     * Проверяет наличие в поле выигрышных комбинаций.
+     * @param forX выбор победы, true - для крестиков, false - для ноликов
      * @return результат проверки.
      */
-    public boolean isWinnerX() {
+    public boolean isWin(boolean forX) {
         int size = table.length;
         boolean result;
         int count;
         result = true;
+        boolean[][] winCell = new boolean[size][size];
+        for (int out = 0; out < size; out++) {
+            for (int in = 0; in < size; in++) {
+                if (forX) {
+                    winCell[out][in] = table[out][in].hasMarkX();
+                } else {
+                    winCell[out][in] = table[out][in].hasMarkO();
+                }
+            }
+        }
         for (int out = 1; out < size; out++) {
-            if (!(table[0][0].hasMarkX() && table[out][out].hasMarkX())) {
+            if (!(winCell[0][0] && winCell[out][out])) {
                 result = false;
                 break;
             }
@@ -30,7 +41,7 @@ public class Logic3T {
         if (!result) {
             result = true;
             for (int out = 1; out < size; out++) {
-                if (!(table[0][size - 1].hasMarkX() && table[out][size - out - 1].hasMarkX())) {
+                if (!(winCell[0][size - 1] && winCell[out][size - out - 1])) {
                     result = false;
                     break;
                 }
@@ -41,7 +52,7 @@ public class Logic3T {
                 result = true;
                 count = 1;
                 for (int in = 1; in < size; in++) {
-                    if (!(table[out][0].hasMarkX() && table[out][in].hasMarkX())) {
+                    if (!(winCell[out][0] && winCell[out][in])) {
                         result = false;
                         break;
                     } else {
@@ -58,7 +69,7 @@ public class Logic3T {
                 result = true;
                 count = 1;
                 for (int in = 1; in < size; in++) {
-                    if (!(table[0][out].hasMarkX() && table[in][out].hasMarkX())) {
+                    if (!(winCell[0][out] && winCell[in][out])) {
                         result = false;
                         break;
                     } else {
@@ -74,64 +85,19 @@ public class Logic3T {
     }
 
     /**
+     * Проверяет наличие в поле выигрышных комбинаций для крестика.
+     * @return результат проверки.
+     */
+    public boolean isWinnerX() {
+        return isWin(true);
+    }
+
+    /**
      * Проверяет наличие в поле выигрышных комбинаций для нолика.
      * @return результат проверки.
      */
     public boolean isWinnerO() {
-        int size = table.length;
-        boolean result;
-        int count;
-        result = true;
-        for (int out = 1; out < size; out++) {
-            if (!(table[0][0].hasMarkO() && table[out][out].hasMarkO())) {
-                result = false;
-                break;
-            }
-        }
-        if (!result) {
-            result = true;
-            for (int out = 1; out < size; out++) {
-                if (!(table[0][size - 1].hasMarkO() && table[out][size - out - 1].hasMarkO())) {
-                    result = false;
-                    break;
-                }
-            }
-        }
-        if (!result) {
-            for (int out = 0; out < size; out++) {
-                result = true;
-                count = 1;
-                for (int in = 1; in < size; in++) {
-                    if (!(table[out][0].hasMarkO() && table[out][in].hasMarkO())) {
-                        result = false;
-                        break;
-                    } else {
-                        count++;
-                    }
-                }
-                if (count == size) {
-                    break;
-                }
-            }
-        }
-        if (!result) {
-            for (int out = 0; out < size; out++) {
-                result = true;
-                count = 1;
-                for (int in = 1; in < size; in++) {
-                    if (!(table[0][out].hasMarkO() && table[in][out].hasMarkO())) {
-                        result = false;
-                        break;
-                    } else {
-                        count++;
-                    }
-                }
-                if (count == size) {
-                    break;
-                }
-            }
-        }
-        return result;
+        return isWin(false);
     }
 
     /**

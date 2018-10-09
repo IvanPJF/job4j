1c4774bd0992b4260914345f7bc5e06d7059d6d8
IvanPJF <teaching-light@yandex.ru>
2018-10-09 14:33:40 +0300

    002.8. Кофемашина. [#34741] Коррекция 1

--- chapter_002/src/main/java/ru/job4j/coffee/CoffeeMachine.java
+++ chapter_002/src/main/java/ru/job4j/coffee/CoffeeMachine.java
@@ -1,15 +1,14 @@
 package ru.job4j.coffee;
 
+import java.util.Arrays;
+
 /**Кофемашина.
  *@author IvanPJF (teaching-light@yandex.ru)
  *@since 08.10.2018
  *@version 0.1
  */
 public class CoffeeMachine {
-    private static final int TEN = 10;
-    private static final int FIVE = 5;
-    private static final int TWO = 2;
-    private static final int ONE = 1;
+    private final int[] coins = {10, 5, 2, 1};
 
     /**
      * Расчет сдачи.
@@ -18,30 +17,20 @@ public class CoffeeMachine {
      * @return Сдача.
      */
     public int[] changes(int value, int price) {
-        int mix = value - price;
-        int ten = mix / TEN;
-        mix = mix % TEN;
-        int five = mix / FIVE;
-        mix = mix % FIVE;
-        int two = mix / TWO;
-        mix = mix % TWO;
-        int one = mix / ONE;
-        int[] change = new int[ten + five + two + one];
-        for (int out = 0; out < change.length; out++) {
-            if (ten > 0) {
-                change[out] = TEN;
-                ten--;
-            } else if (five > 0) {
-                change[out] = FIVE;
-                five--;
-            } else if (two > 0) {
-                change[out] = TWO;
-                two--;
-            } else {
-                change[out] = ONE;
-                one--;
+        int[] result = {};
+        int change = value - price;
+        int temp, tempAll = 0;
+        for (int coin : this.coins) {
+            if (change / coin != 0) {
+                temp = change / coin;
+                tempAll += temp;
+                change = change % coin;
+                result = Arrays.copyOf(result, tempAll);
+                for (int in = tempAll - temp; in < tempAll; in++) {
+                    result[in] = coin;
+                }
             }
         }
-        return change;
+        return result;
     }
 }

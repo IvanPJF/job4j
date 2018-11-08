b961e824883d535443f3dc721805c48feba38b91
IvanPJF <teaching-light@yandex.ru>
2018-11-08 23:11:50 +0300

    004.2.4. В задании шахматы для поиска использовать Stream API. [#24257]

--- chapter_002/src/main/java/ru/job4j/chess/Chess.java
+++ chapter_002/src/main/java/ru/job4j/chess/Chess.java
@@ -18,6 +18,8 @@ import ru.job4j.chess.firuges.ImpossibleMoveException;
 import ru.job4j.chess.firuges.black.*;
 import ru.job4j.chess.firuges.white.*;
 
+import java.util.Arrays;
+
 public class Chess extends Application {
     private static final String JOB4J = "Шахматы на www.job4j.ru";
     private final int size = 8;
@@ -168,15 +170,10 @@ public class Chess extends Application {
     }
 
     private Cell findBy(double graphX, double graphY) {
-        Cell rst = Cell.A1;
         int x = (int) graphX / 40;
         int y = (int) graphY / 40;
-        for (Cell cell : Cell.values()) {
-            if (cell.x == x && cell.y == y) {
-                rst = cell;
-                break;
-            }
-        }
-        return rst;
+        return Arrays.stream(Cell.values()).filter(
+                cell -> cell.x == x && cell.y == y
+        ).findFirst().orElse(Cell.A1);
     }
 }
--- chapter_002/src/main/java/ru/job4j/chess/Logic.java
+++ chapter_002/src/main/java/ru/job4j/chess/Logic.java
@@ -4,6 +4,9 @@ import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
 import ru.job4j.chess.firuges.ImpossibleMoveException;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 /**
  *
  *
@@ -26,13 +29,11 @@ public class Logic {
         }
         boolean rst = false;
         Cell[] steps = this.figures[index].way(source, dest);
-        for (Figure figure : this.figures) {
-            for (Cell step : steps) {
-                if (figure.position().equals(step)) {
-                    throw new OccupiedWayException("Путь не свободен.");
-                }
-            }
-        }
+        Arrays.stream(this.figures).filter(
+                figure -> Arrays.stream(steps).anyMatch(step -> figure.position().equals(step))
+        ).forEach(figure -> {
+            throw new OccupiedWayException("Путь не свободен.");
+        });
         if (steps.length > 0) {
             this.figures[index] = this.figures[index].copy(dest);
             rst = true;
@@ -41,20 +42,13 @@ public class Logic {
     }
 
     public void clean() {
-        for (int position = 0; position != this.figures.length; position++) {
-            this.figures[position] = null;
-        }
+        IntStream.range(0, this.figures.length).forEach(position -> this.figures[position] = null);
         this.index = 0;
     }
 
     private int findBy(Cell cell) {
-        int rst = -1;
-        for (int index = 0; index != this.figures.length; index++) {
-            if (this.figures[index] != null && this.figures[index].position().equals(cell)) {
-                rst = index;
-                break;
-            }
-        }
-        return rst;
+        return IntStream.range(0, this.figures.length).filter(
+                ind -> this.figures[ind] != null && this.figures[ind].position().equals(cell)
+        ).findFirst().orElse(-1);
     }
 }

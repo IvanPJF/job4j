51b369e1a394573cd12829478c3264c02c4d0007
IvanPJF <teaching-light@yandex.ru>
2019-03-04 22:42:10 +0300

    007.3.1. Хранилище машин [#1733]

---
+++ chapter_007/src/main/resources/storage_cars/initial_storage_cars.sql
@@ -0,0 +1,29 @@
+﻿
+CREATE TABLE body (
+	id serial PRIMARY KEY,
+	name varchar(50)
+);
+CREATE TABLE engine (
+	id serial PRIMARY KEY,
+	name varchar(50)
+);
+CREATE TABLE transmission (
+	id serial PRIMARY KEY,
+	name varchar(50)
+);
+CREATE TABLE car(
+	id serial PRIMARY KEY,
+	name varchar(50),
+	body_id int NOT NULL REFERENCES body(id),
+	engine_id int NOT NULL REFERENCES engine(id),
+	transmission_id int NOT NULL REFERENCES transmission(id)
+);
+
+INSERT INTO body (name) VALUES ('sedan'), ('station wagon'), ('hatchback'), ('coupe'), ('fastback');
+INSERT INTO engine (name) VALUES ('diesel'), ('petrol'), ('fuel injection'), ('electromotor');
+INSERT INTO transmission (name) VALUES ('manual'), ('automatica'), ('other');
+INSERT INTO car (name, body_id, engine_id, transmission_id) VALUES
+	('MITSUBISHI LANCER', 1, 3, 2),
+	('MAZDA 3', 3, 3, 2),
+	('Audi A5', 4, 1, 1),
+	('Tesla Model S', 5, 4, 2);
---
+++ chapter_007/src/main/resources/storage_cars/queries_storage_cars.sql
@@ -0,0 +1,9 @@
+﻿
+SELECT c.name, b.name body, e.name engine, t.name transmission FROM car c
+LEFT OUTER JOIN body b ON c.body_id = b.id
+LEFT OUTER JOIN engine e ON c.engine_id = e.id
+LEFT OUTER JOIN transmission t ON c.transmission_id = t.id;
+
+SELECT b.name FROM car c RIGHT OUTER JOIN body b ON c.body_id = b.id WHERE c.id IS NULL;
+SELECT e.name FROM car c RIGHT OUTER JOIN engine e ON c.engine_id = e.id WHERE c.id IS NULL;
+SELECT t.name FROM car c RIGHT OUTER JOIN transmission t ON c.transmission_id = t.id WHERE c.id IS NULL;

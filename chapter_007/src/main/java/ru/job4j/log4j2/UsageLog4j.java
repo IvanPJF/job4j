0d30dccf20e19c8bf0955c04e7229c9817431a82
IvanPJF <teaching-light@yandex.ru>
2019-03-14 22:03:27 +0300

    007.4.1. Log4j 2. Логирование системы. [#95335] Коррекция 1

--- chapter_007/src/main/java/ru/job4j/log4j2/UsageLog4j.java
+++ chapter_007/src/main/java/ru/job4j/log4j2/UsageLog4j.java
@@ -8,10 +8,11 @@ public class UsageLog4j {
     private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        int version = 1;
+        LOG.trace("trace message {}", version);
+        LOG.debug("debug message {}", version);
+        LOG.info("info message {}", version);
+        LOG.warn("warn message {}", version);
+        LOG.error("error message {}", version);
     }
 }

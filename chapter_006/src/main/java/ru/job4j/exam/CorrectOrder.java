aa01a8a503a7646fb63e55629eb9c718bfdd6c17
IvanPJF <teaching-light@yandex.ru>
2019-05-17 14:18:04 +0300

    006.3.2(Exam). DB Scripts [#138867] Коррекция 1

--- chapter_006/src/main/java/ru/job4j/exam/CorrectOrder.java
+++ chapter_006/src/main/java/ru/job4j/exam/CorrectOrder.java
@@ -1,15 +1,13 @@
 package ru.job4j.exam;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Define the order in which scripts are run.
- *@author IvanPJF (teaching-light@yandex.ru)
- *@since 07.05.2019
- *@version 0.1
+ *
+ * @author IvanPJF (teaching-light@yandex.ru)
+ * @version 0.1
+ * @since 07.05.2019
  */
 public class CorrectOrder {
 
@@ -20,7 +18,6 @@ public class CorrectOrder {
 
     /**
      * The color of the vertices.
-     * 0 - white(vertex not checked), 1 - black(vertex checked).
      **/
     private HashMap<Integer, Integer> vertColor;
 
@@ -29,6 +26,21 @@ public class CorrectOrder {
      */
     private final List<Integer> sortScript;
 
+    /**
+     * Vertex not checked.
+     */
+    private static final int WHITE = 0;
+
+    /**
+     * The vertex is added to the stack.
+     */
+    private static final int GRAY = 1;
+
+    /**
+     * Vertex checked.
+     */
+    private static final int BLACK = 2;
+
     public CorrectOrder(List<VulnerabilityScript> scriptsList) {
         this.scriptsToHash(scriptsList);
         this.sortScript = new ArrayList<>(scriptsList.size());
@@ -36,7 +48,7 @@ public class CorrectOrder {
 
     /**
      * Vertices and their dependencies are entered in HashMap.
-     * Each vertex is colored white(0).
+     * Each vertex is colored white.
      *
      * @param scriptList List of unsorted scripts.
      */
@@ -46,7 +58,7 @@ public class CorrectOrder {
         this.vertColor = new HashMap<>(sizeHash);
         for (VulnerabilityScript value : scriptList) {
             this.vertDepend.put(value.getScriptId(), value.getDependencies());
-            this.vertColor.put(value.getScriptId(), 0);
+            this.vertColor.put(value.getScriptId(), WHITE);
         }
     }
 
@@ -57,26 +69,59 @@ public class CorrectOrder {
      */
     public List<Integer> sort() {
         Set<Integer> vertKey = this.vertDepend.keySet();
-        for (int vertex : vertKey) {
-            this.dfs(vertex);
+        Deque<Integer> stack = new LinkedList<>();
+        for (Integer vertex : vertKey) {
+            if (this.vertColor.get(vertex) == WHITE) {
+                if (this.hasDependencies(vertex)) {
+                    stack.push(vertex);
+                    this.vertColor.replace(vertex, GRAY);
+                    for (Integer depend : this.vertDepend.get(vertex)) {
+                        if (this.vertColor.get(depend) == WHITE) {
+                            stack.push(depend);
+                            this.vertColor.replace(depend, GRAY);
+                        }
+                    }
+                } else {
+                    this.sortScript.add(vertex);
+                    this.vertColor.replace(vertex, BLACK);
+                }
+            }
+            this.parseStack(stack);
         }
         return this.sortScript;
     }
 
     /**
-     * Determining the order in which scripts are run.
+     * Check the stack items to see if they can be added to the result list.
      *
-     * @param vertex Vertex to check.
+     * @param stack
      */
-    private void dfs(int vertex) {
-        if (this.vertColor.get(vertex) == 0) {
-            if (this.vertDepend.get(vertex) != null) {
-                for (int depend : this.vertDepend.get(vertex)) {
-                    dfs(depend);
+    private void parseStack(Deque<Integer> stack) {
+        while (!stack.isEmpty()) {
+            Integer stackElem = stack.peek();
+            if (this.vertColor.get(stackElem) == GRAY) {
+                if (this.hasDependencies(stackElem)) {
+                    for (Integer value : this.vertDepend.get(stackElem)) {
+                        if (this.vertColor.get(value) != BLACK) {
+                            break;
+                        }
+                    }
                 }
+                this.sortScript.add(stack.pop());
+                this.vertColor.replace(stackElem, BLACK);
+            } else {
+                stack.pop();
             }
-            this.sortScript.add(vertex);
-            this.vertColor.replace(vertex, 1);
         }
+    }
+
+    /**
+     * Check whether the vertex has dependencies.
+     *
+     * @param vertex
+     * @return
+     */
+    private boolean hasDependencies(Integer vertex) {
+        return this.vertDepend.get(vertex) != null && !this.vertDepend.get(vertex).isEmpty();
     }
 }
